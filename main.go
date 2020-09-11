0e60b8ae2b5d4cefae81cfd1a62a031eb9a355b0
evaldasNe <neevaldas@gmail.com>
2020-09-11 19:29:34 +0300

    init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Person struct
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, "Hello World!")
+	})
+
+	router.GET("/test", func(c *gin.Context) {
+		users := make([]Person, 3)
+		users[0] = Person{Name: "Bill", Age: 44}
+		users[1] = Person{Name: "Jon", Age: 16}
+		users[2] = Person{Name: "Joe", Age: 66}
+
+		c.JSON(200, gin.H{
+			"users": users,
+		})
+	})
+	router.Run(":80")
+}
